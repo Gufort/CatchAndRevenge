84abdc289bd3d9157434e3436d58366a3cd7cc44
Игорь Аведисьян <guffy911d@gmail.com>
2024-11-08 20:48:27 +0300

    Add new scene

---
+++ Assets/Scripts/KeyTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyTrigger : MonoBehaviour
+{
+    public GameObject panel;
+    private bool trigger;
+
+    private void OnTriggerEnter2D(Collider2D other){
+        panel.SetActive(true);
+        trigger = true;
+    }
+    private void OnTriggerExit2D(Collider2D other){
+        panel.SetActive(false);
+        trigger = false;
+    }
+
+
+    void Start(){
+        panel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (trigger && Input.GetKeyDown(KeyCode.E))
+        {
+            LevelChange.FadeToLevel();
+        }
+    }
+}
---
+++ Assets/Scripts/LevelChange.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class LevelChange : MonoBehaviour
+{
+    public static Animator anim;
+    public int level_number;
+    public Vector3 position;
+    public VectorValue playerStorage;
+
+    private void Start(){
+        anim = GetComponent<Animator>();
+    }
+
+    public static void FadeToLevel(){
+        anim.SetTrigger("Fade");
+    }
+
+    public void OnFadeComplete(){
+        playerStorage.initialValue = position;
+        SceneManager.LoadScene(level_number);
+    }
+}
