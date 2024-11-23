d68416985a170e0d26777b6246866c9b59834766
Игорь Аведисьян <guffy911d@gmail.com>
2024-11-23 18:49:02 +0300

    Add ParallaxEffect

---
+++ Assets/Scripts/ParallaxEffect/ParallaxBackground.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class ParallaxBackground : MonoBehaviour
+{
+    public ParallaxCamera parallaxCamera;
+    private List<ParallaxLayer> parallaxLayers = new List<ParallaxLayer>();
+
+    void Start()
+    {
+        if (parallaxCamera == null)
+            parallaxCamera = Camera.main.GetComponent<ParallaxCamera>();
+
+        if (parallaxCamera != null)
+            parallaxCamera.onCameraTranslate += Move;
+
+        SetLayers();
+    }
+
+    void SetLayers()
+    {
+        parallaxLayers.Clear();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            ParallaxLayer layer = transform.GetChild(i).GetComponent<ParallaxLayer>();
+
+            if (layer != null)
+            {
+                layer.name = "Layer-" + i;
+                parallaxLayers.Add(layer);
+            }
+        }
+    }
+
+    void Move(float delta)
+    {
+        foreach (ParallaxLayer layer in parallaxLayers)
+        {
+            layer.Move(delta);
+        }
+    }
+}
