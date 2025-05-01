3430b4f9fbff9a01eb211cd58a2557d44af4ccbc
Игорь Аведисьян <guffy911d@gmail.com>
2025-05-01 14:12:51 +0300

    Add NarratorAttack

---
+++ Assets/Scripts/Narrator/NarratorScriptableObjects.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class NarratorScriptableObjects : ScriptableObject
+{
+    public int damage = 10;
+    public int curr_hp = 100;
+}
