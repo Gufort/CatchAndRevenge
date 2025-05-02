ce9770c40b1c5205b2beda03f95ae9eef51df9f2
Игорь Аведисьян <guffy911d@gmail.com>
2025-05-02 14:43:28 +0300

    Add FanAttack for Narrator

--- Assets/Scripts/Narrator/NarratorAttack.cs
+++ Assets/Scripts/Narrator/NarratorAttack.cs
@@ -10,6 +10,7 @@ public class NarratorAttack : MonoBehaviour
     [SerializeField] private Transform _attackPoint;
     [SerializeField] private GameObject _prefabFireBoll;
     [SerializeField] private AudioClip _soundClip;
+    [SerializeField] private float _spreedAngle = 30f;
     private Animator _animator;
     private bool _isAttacking = false;
     private AudioSource _audioSource;
@@ -23,24 +24,37 @@ public class NarratorAttack : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
-    private void Attack(){
+    private void StandartAttack(){
         _lastShotTime = Time.time;
         GameObject fireBoll = Instantiate(_prefabFireBoll,_attackPoint.position, Quaternion.identity);
-        FireBollScript arrow = fireBoll.GetComponent<FireBollScript>();
+        FireBollScript boll = fireBoll.GetComponent<FireBollScript>();
 
         Vector2 direction = (PlayerController.instance.transform.position - _attackPoint.position).normalized;
 
         _animator.SetFloat("HorizontalAttack", direction.x);
         _animator.SetFloat("VerticalAttack", direction.y);
 
-        arrow.setDirection(direction);
+        boll.setDirection(direction);
+        _animator.SetBool("Attack", false);
+    }
+
+    private void FanAttack(){
+        _lastShotTime = Time.time;
+        Vector2 direction = (PlayerController.instance.transform.position - _attackPoint.position).normalized;
+        for(int i = 0; i < 3; ++i){
+            float angle = _spreedAngle*(i/2f - 0.5f); //угол между фаерболами
+            Vector2 devDirection = Quaternion.Euler(0,0,angle) * direction;
+            GameObject fireBoll = Instantiate(_prefabFireBoll,_attackPoint.position, Quaternion.identity);
+            FireBollScript boll = fireBoll.GetComponent<FireBollScript>();
+            boll.setDirection(devDirection);
+        }
         _animator.SetBool("Attack", false);
     }
 
     private IEnumerator WaitForSoundAndAttack()
     {
         yield return new WaitForSeconds(0.8f);
-        Attack();
+        FanAttack();
         _isAttacking = false; 
     }
 
