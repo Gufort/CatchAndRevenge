5ee0775b9a4ccda9f7ecf7652987e7b4d5742548
Грищук Александр <grishchuk@sfedu.ru>
2025-05-04 14:09:10 +0300

    Add dialogues in wasteland. Add Final Bridge Scene and dialogue in it

--- Assets/Scripts/Level/EnemyInWasteland.cs
+++ Assets/Scripts/Level/EnemyInWasteland.cs
@@ -6,39 +6,40 @@ public class EnemyInWasteland : MonoBehaviour
 {
      [SerializeField] private List<EnemyHP> _arrayOfBandits;
     [SerializeField] private List<ArcherHP> _arrayOfArcher;
-    //[SerializeField] private GameObject _collider;
+    [SerializeField] private GameObject _collider;
+    [SerializeField] private GameObject _colliderOld;
     private int countOfEnemies = 14;
 
     private void Start()
     {
-        //_collider.SetActive(false);
+        _collider.SetActive(false);
         countOfEnemies = PlayerPrefs.GetInt("CountEnemyInWasteland", 14);  
     }
 
     private void Update()
     {
-       // if (countOfEnemies == 0)
-            //_collider.SetActive(true);
-        // else
-        // {
-           
-        // }
-
-         foreach(var archer in _arrayOfArcher.ToArray())
-                if(archer._isDie){
-                    _arrayOfArcher.Remove(archer);
-                    countOfEnemies--;
-                    PlayerPrefs.SetInt("CountEnemyInWasteland",countOfEnemies);
-                    PlayerPrefs.Save();
-                }
-            
-            foreach(var bandit in _arrayOfBandits.ToArray())
-                if(bandit._isDead){
-                    _arrayOfBandits.Remove(bandit);
-                    countOfEnemies--;
-                    PlayerPrefs.SetInt("CountEnemyInWasteland",countOfEnemies);  
-                    PlayerPrefs.Save();      
-                }
+        if (countOfEnemies == 0){
+            _collider.SetActive(true);
+            _colliderOld.SetActive(false);
+        }
+        else
+        {
+            foreach(var archer in _arrayOfArcher.ToArray())
+                    if(archer._isDie){
+                        _arrayOfArcher.Remove(archer);
+                        countOfEnemies--;
+                        PlayerPrefs.SetInt("CountEnemyInWasteland",countOfEnemies);
+                        PlayerPrefs.Save();
+                    }
+                
+                foreach(var bandit in _arrayOfBandits.ToArray())
+                    if(bandit._isDead){
+                        _arrayOfBandits.Remove(bandit);
+                        countOfEnemies--;
+                        PlayerPrefs.SetInt("CountEnemyInWasteland",countOfEnemies);  
+                        PlayerPrefs.Save();      
+                    }
+        }
     }
 
     public int getCount() { return countOfEnemies; }
